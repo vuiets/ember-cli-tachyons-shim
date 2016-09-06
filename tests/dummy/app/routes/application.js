118f0e79878a763f752d323e0ff2e70c378b5a61
Vishak Partha <wizvishak@gmail.com>
2016-09-07 02:34:53 +0530

    o boy we need a dummy app (#5)
    * Remove stray `node-tests` dir
    * Add `dummy` app

---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,15 @@
+import Route from 'ember-route';
+import injectService from 'ember-service/inject';
+
+export default Route.extend({
+	ajax: injectService(),
+
+	model() {
+		const director = 'Christopher Nolan',
+			netflixAPI = `http://netflixroulette.net/api/api.php?director=${director}`;
+
+		return this.get('ajax').request(netflixAPI, {
+			method: 'GET'
+		});
+	}
+});
