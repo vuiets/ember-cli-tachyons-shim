a7ead6ba6b05cd728ad4c3421bcc78adf141a224
Vishak Partha <wizvishak@gmail.com>
2016-09-18 13:25:19 +0530

    tachyons v4.5.1 (#24)
    Quick fire releases.

--- blueprints/ember-cli-tachyons-shim/index.js
+++ blueprints/ember-cli-tachyons-shim/index.js
@@ -8,7 +8,7 @@ module.exports = {
 
 	afterInstall: function (options) {
 		var promise = new Promise((resolve, reject) => {
-			this.addPackageToProject('tachyons', '4.5.0').then((success) => {
+			this.addPackageToProject('tachyons', '4.5.1').then((success) => {
 				try {
 					var fs = require('fs');
 
