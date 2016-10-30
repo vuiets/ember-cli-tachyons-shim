665eccb6608d20d7c8f77f807d6ff7e0583faee9
Visahavel <wizvishak@gmail.com>
2016-10-30 20:44:16 +0530

    4.5.4 (#39)
    * Bump version to 4.5.4
    * ember-cli@2.9.1

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,46 +1,51 @@
 /* jshint node: true */
 
-module.exports = function(environment) {
-  var ENV = {
-    modulePrefix: 'dummy',
-    environment: environment,
-    rootURL: '/',
-    locationType: 'auto',
-    EmberENV: {
-      FEATURES: {
-        // Here you can enable experimental features on an ember canary build
-        // e.g. 'with-controller': true
-      }
-    },
+module.exports = function (environment) {
+	var ENV = {
+		modulePrefix: 'dummy',
+		environment: environment,
+		rootURL: '/',
+		locationType: 'auto',
+		EmberENV: {
+			FEATURES: {
+				// Here you can enable experimental features on an ember canary build
+				// e.g. 'with-controller': true
+			}
+		},
 
-    APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
-    }
-  };
+		EXTEND_PROTOTYPES: {
+			// Prevent Ember Data from overriding Date.parse.
+			Date: false
+		},
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
+		APP: {
+			// Here you can pass flags/options to your application instance
+			// when it is created
+		}
+	};
 
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.locationType = 'none';
+	if (environment === 'development') {
+		// ENV.APP.LOG_RESOLVER = true;
+		// ENV.APP.LOG_ACTIVE_GENERATION = true;
+		// ENV.APP.LOG_TRANSITIONS = true;
+		// ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+		// ENV.APP.LOG_VIEW_LOOKUPS = true;
+	}
 
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
+	if (environment === 'test') {
+		// Testem prefers this...
+		ENV.locationType = 'none';
 
-    ENV.APP.rootElement = '#ember-testing';
-  }
+		// keep test console output quieter
+		ENV.APP.LOG_ACTIVE_GENERATION = false;
+		ENV.APP.LOG_VIEW_LOOKUPS = false;
 
-  if (environment === 'production') {
+		ENV.APP.rootElement = '#ember-testing';
+	}
 
-  }
+	if (environment === 'production') {
 
-  return ENV;
+	}
+
+	return ENV;
 };
