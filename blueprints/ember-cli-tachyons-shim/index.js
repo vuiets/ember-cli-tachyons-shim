22da289dcc207b2b9fa7e009ef7bffd8fbaae564
Vishak <wizvishak@gmail.com>
2017-09-30 15:27:33 +0530

    tachyons@4.8.0

--- blueprints/ember-cli-tachyons-shim/index.js
+++ blueprints/ember-cli-tachyons-shim/index.js
@@ -8,7 +8,7 @@ module.exports = {
 
 	afterInstall: function (options) {
 		var promise = new Promise((resolve, reject) => {
-			this.addPackageToProject('tachyons', '4.7.4').then((success) => {
+			this.addPackageToProject('tachyons', '4.8.0').then((success) => {
 				try {
 					var fs = require('fs');
 
